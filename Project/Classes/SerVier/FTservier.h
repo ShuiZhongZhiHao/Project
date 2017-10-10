5be0f9f0a7a120937178ac06f0bf37a4b18d60e2
浩李 <516070356@qq.com>
2017-10-10 21:24:21 +0800

    New file

---
+++ Project/Classes/SerVier/FTservier.h
@@ -0,0 +1,12 @@
+//
+//  FTservier.h
+//  Pods-Project_Example
+//
+//  Created by 浩李 on 2017/10/8.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FTservier : NSObject
+-(void)dog;
+@end
---
+++ Project/Classes/SerVier/FTservier.m
@@ -0,0 +1,15 @@
+//
+//  FTservier.m
+//  Pods-Project_Example
+//
+//  Created by 浩李 on 2017/10/8.
+//
+
+#import "FTservier.h"
+
+@implementation FTservier
+-(void)dog
+{
+    NSLog(@"dog");
+}
+@end
